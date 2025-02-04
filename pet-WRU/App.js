972f60f005ff7017feed9340243854ad9df5b05a
EthanPortelli <159474174+EthanPortelli@users.noreply.github.com>
2025-02-04 18:21:59 -0500

    git commit

--- pet-WRU/App.js
+++ pet-WRU/App.js
@@ -3,7 +3,7 @@ const path=require('path');
 const app = express();
 
 const PORT= process.env.PORT || 3000;
-
+ 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname + '/mainpage.html'));
 });
