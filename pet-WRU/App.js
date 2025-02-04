b5ee47b54a6bf4d84bbba8bf64c07c691186e0fa
jen <91272778+jennefercampoverde@users.noreply.github.com>
2025-02-04 01:28:07 -0500

    Added index.html, updated .gitignore, and removed .env from Git tracking for testing

---
+++ pet-WRU/App.js
@@ -0,0 +1,12 @@
+const express = require(`express`);
+const path=require('path');
+const app = express();
+
+const PORT= process.env.PORT || 3000;
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//listen for specific port
+app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
