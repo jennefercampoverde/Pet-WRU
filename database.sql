226b0c7d9813bd33b28139778bbe02743e794c91
jen <91272778+jennefercampoverde@users.noreply.github.com>
2025-02-03 21:05:03 -0500

    SQL file for database creation added

---
+++ database.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE websiteDatabase;
+
+use websiteStorage;
+
+CREATE TABLE usersInfo(
+    userID int NOT NULL AUTOINCREMENT,
+    UserName varchar(255) NOT NULL,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    DOB Date NOT NULL,
+    emailAddress varchar(255) NOT NULL,
+    userPassword varchar(255) NOT NULL,
+    Zipcode varchar(5) NOT NULL,
+    City varchar(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE donations(
+    donationID int NOT NULL AUTOINCREMENT,
+    userID int NOT NULL,
+    Zipcode varchar(5) NOT NULL, 
+    status varchar(255) NOT NULL, 
+    itemName varchar(255) NOT NULL,
+    condition varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    quantity int NOT NULL,
+    PRIMARY KEY (donationID),
+    FOREIGN KEY(userID) REFERENCES usersInfo (userID)
+
+);
+
+CREATE TABLE lostPets(
+    lostID  int NOT NULL AUTOINCREMENT,
+    userID int NOT NULL, 
+    dateCreated Date NOT NULL,
+    dateLost Date NOT NULL,
+    lastZipcode varchar(5) NOT NULL,
+    lastCityID varchar(255) NOT NULL,
+    petName varchar(255) NOT NULL,
+    animalType varchar(255) NOT NULL,
+    animalSize varchar(255) NOT NULL,
+    animalColor varchar(255) NOT NULL,
+    animalGender varchar(1) NOT NULL,
+    animal_image_path varcahr(255),  
+    description varchar(255),
+    status varchar(255) NOT NULL, 
+    PRIMARY KEY (lostID),
+    FOREIGN KEY(userID) REFERENCES usersInfo (userID)
+);
+
+CREATE TABLE foundPets(
+    foundID int NOT NULL AUTOINCREMENT,
+    lostID int NOT NULL,
+    dateFound date NOT NULL,
+    status varchar(255) NOT NULL,
+    PRIMARY KEY(foundID),
+    FOREIGN KEY(lostID) REFERENCES lostPets(lostID)
+    -- I don't think we need the following because then it would automatically update to "lost" -> FOREIGN KEY(status) REFERENCES lostPets(status) ON UPDATE CASCADE;   
+);
