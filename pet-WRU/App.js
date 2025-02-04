8c9e76e304b13ed4d5db129146d457fbc54552ec
jen <91272778+jennefercampoverde@users.noreply.github.com>
2025-02-04 01:37:42 -0500

    Combined frontend branch files and ignored .env

--- pet-WRU/App.js
+++ pet-WRU/App.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT= process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/mainpage.html'));
 });
 
 //listen for specific port
