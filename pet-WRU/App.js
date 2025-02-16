6e7459d24b3fbb625aa59c752f8540414a2bdb28
Ethan Portelli <159474174+EthanPortelli@users.noreply.github.com>
2025-02-16 12:47:13 -0500

    added session tracking via express-session

--- pet-WRU/App.js
+++ pet-WRU/App.js
@@ -1,17 +1,26 @@
-const express = require(`express`);
+const express = require('express');
 const path = require('path');
-const app = express();
+const session = require('express-session');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 const Routes = require("./Routes");
 
+// Configure express-session
+app.use(session({
+    secret: 'secret_key', 
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true } 
+}));
 
 // Middleware for parsing URL-encoded and JSON data
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files (CSS, JS, images)
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname)));
 
 // Use routes
