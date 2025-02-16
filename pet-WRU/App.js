f40a85c166a48df0469fff6657077583369bedf8
EthanPortelli <159474174+EthanPortelli@users.noreply.github.com>
2025-02-16 13:01:28 -0500

    git commit

--- pet-WRU/App.js
+++ pet-WRU/App.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-
-const app = express();
+const app = express(); 
 const PORT = process.env.PORT || 3000;
-
 const Routes = require("./Routes");
 
 // Configure express-session
