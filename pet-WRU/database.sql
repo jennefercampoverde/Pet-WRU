17dfb870e7352f69b368ee0ba2dd825a46cae712
jen <91272778+jennefercampoverde@users.noreply.github.com>
2025-02-20 10:49:03 -0500

    Updating Database with strict ENUM values for specific columns and added donations table

--- pet-WRU/database.sql
+++ pet-WRU/database.sql
@@ -7,46 +7,33 @@ USE websiteDatabase;
 
 CREATE TABLE usersInfo(
     userID int NOT NULL AUTO_INCREMENT,
-    UserName varchar(255) NOT NULL,
-    FirstName varchar(255) NOT NULL,
-    LastName varchar(255) NOT NULL,
-    DOB Date NOT NULL,
+    userName varchar(255) NOT NULL,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    dob Date NOT NULL,
     emailAddress varchar(255) NOT NULL,
     userPassword varchar(255) NOT NULL,
-    Zipcode varchar(5) NOT NULL,
-    City varchar(255) NOT NULL,
+    zipcode varchar(5) NOT NULL,
+    city varchar(255) NOT NULL,
     PRIMARY KEY (userID)
 );
 
-/*CREATE TABLE donations(
-    donationID int NOT NULL AUTO_INCREMENT,
-    userID int NOT NULL,
-    Zipcode varchar(5) NOT NULL, 
-    status varchar(255) NOT NULL, 
-    itemName varchar(255) NOT NULL,
-    quantity int NOT NULL,
-    condition varchar(255) NOT NULL,
-    description varchar(255) NOT NULL,
-    item_image_path varcahr(255), 
-    PRIMARY KEY (donationID),
-    FOREIGN KEY(userID) REFERENCES usersInfo (userID)
-);*/
 
 CREATE TABLE lostPets(
     lostID  int NOT NULL AUTO_INCREMENT,
     userID int NOT NULL,
-    dateCreated Date NOT NULL,
+    dateCreated DateTime DEFAULT NOW(),
     dateLost Date NOT NULL,
     lastZipcode varchar(5) NOT NULL,
     lastCityID varchar(255) NOT NULL,
     petName varchar(255) NOT NULL,
-    animalType varchar(255) NOT NULL,
-    animalSize varchar(255) NOT NULL,
-    animalColor varchar(255) NOT NULL,
-    animalGender varchar(1) NOT NULL,
+    animalType ENUM('Bird', 'Cat', 'Chickens', 'Dog', 'Ferrets', 'Gerbils', 'Guinea Pigs', 'Hamster', 'Mice', 'Turtles','Reptiles') NOT NULL,
+    animalSize ENUM('XS','S','M','L','XL','XXL') NOT NULL,
+    animalColor ENUM('Black', 'Blue', 'Brown', 'Gray', 'Green', 'Orange', 'Red', 'Silver', 'Tan', 'White', 'Yellow') NOT NULL,
+    animalGender ENUM('Female','Male') NOT NULL,
     animal_image_path varchar(255),  
     description varchar(255),
-    status varchar(255) NOT NULL, 
+    status ENUM('Found','Lost') NOT NULL, 
     PRIMARY KEY (lostID),
     FOREIGN KEY(userID) REFERENCES usersInfo (userID)
 );
@@ -55,8 +42,34 @@ CREATE TABLE foundPets(
     foundID int NOT NULL AUTO_INCREMENT,
     lostID int NOT NULL,
     dateFound date NOT NULL,
-    status varchar(255) NOT NULL,
+    status ENUM('Lost','Found') NOT NULL,
     PRIMARY KEY(foundID),
-    FOREIGN KEY(lostID) REFERENCES lostPets(lostID)
-    -- I don't think we need the following because then it would automatically update to "lost" -> FOREIGN KEY(status) REFERENCES lostPets(status) ON UPDATE CASCADE;   
+    FOREIGN KEY(lostID) REFERENCES lostPets(lostID)  
+);
+
+CREATE TABLE postComments(
+    commentID int NOT NULL AUTO_INCREMENT,
+    lostID int NOT NULL,
+    dateCreated DateTime DEFAULT NOW() NOT NULL,
+    commentText varchar(255) NOT NULL,
+    userID int NOT NULL, 
+    PRIMARY KEY (commentID),
+    FOREIGN KEY(lostID) REFERENCES lostPets(lostID),
+    FOREIGN KEY (userID) REFERENCES usersInfo(userID)
+);
+
+CREATE TABLE donations(
+    donationID int NOT NULL AUTO_INCREMENT,
+    userID int NOT NULL,
+    dateCreated DateTime DEFAULT NOW(),
+    zipcode varchar(5) NOT NULL, 
+    itemStatus ENUM('Available','Pending','Claimed') NOT NULL, 
+    itemCategory ENUM('Bedding','Clothing','Grooming Supplies','Leashes & Collars','Litter & Waste Supplies' 'Medical Supplies','Toys','Transport Supplies') NOT NULL,
+    itemName varchar(255) NOT NULL,
+    quantity int NOT NULL,
+    itemCondition ENUM('New','Like New', 'Gently Used', 'Used', 'Worn') NOT NULL,
+    itemDescription varchar(255) NOT NULL,
+    item_image_path varchar(255), 
+    PRIMARY KEY (donationID),
+    FOREIGN KEY(userID) REFERENCES usersInfo (userID)
 );
